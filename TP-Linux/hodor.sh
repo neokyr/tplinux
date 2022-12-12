ebedd0923cd987a304b31592322b1d90710f3329
neokyr <p00riel.kyrillin@gmail.com>
2022-12-12 09:08:39 -0500

    Add  'Hoddor !' echo and tar creation

--- TP-Linux/hodor.sh
+++ TP-Linux/hodor.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
 #Get percentage of used inode on /dev/sda1 and put it in "usedinode"
-usedinode=$(df -i &> /dev/null | grep '/dev/sda1' | tr -s " " | cut -d " " -f 5 | cut -d "%" -f 1)
+unode=$(df -i | grep '/dev/sda1' | tr -s " " | cut -d " " -f 5 | cut -d "%" -f 1)
 
-
-if (( $usedinode > 80 )) && [ $USER = "root" ]
+if (( $unode > 80 )) && [ $USER = "root" ]
 then
+	# Create archive dir if not exist
+	mkdir -p /var/log/exercice/archives/
 	# Generate a new archive file and store its path
-	tarpath=$(mktmp -p /var/log/exercice/archives/)
+	tarpath=$(mktemp -p /var/log/exercice/archives/ -t "archive.XXXXXXXXXX.tar")
+	# put everything in recent_log in a tar archive
+	tar -cf $tarpath /var/log/exercice/recent_logs/* && sudo rm -f /var/log/exercice/recent_logs/*
+        echo "HODOOOR !!!"
+
 fi
