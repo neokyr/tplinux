bc4e4c0c0f0b841b4de66c543a625bfb8deb7b5d
neokyr <p00riel.kyrillin@gmail.com>
2022-12-12 08:24:11 -0500

    Add new archive creation

--- TP-Linux/hodor.sh
+++ TP-Linux/hodor.sh
@@ -2,3 +2,10 @@
 
 #Get percentage of used inode on /dev/sda1 and put it in "usedinode"
 usedinode=$(df -i &> /dev/null | grep '/dev/sda1' | tr -s " " | cut -d " " -f 5 | cut -d "%" -f 1)
+
+
+if (( $usedinode > 80 )) && [ $USER = "root" ]
+then
+	# Generate a new archive file and store its path
+	tarpath=$(mktmp -p /var/log/exercice/archives/)
+fi
