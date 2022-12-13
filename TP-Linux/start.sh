3cc24c1b65266e1da1201b141032c324f6ff5c79
neokyr <p00riel.kyrillin@gmail.com>
2022-12-13 05:19:48 -0500

    fix error root should'nt have been there

--- TP-Linux/start.sh
+++ TP-Linux/start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-echo "* * * * * root sudo bash $PWD/log.sh 1000'" | crontab -u root -
-(crontab -u root -l ; echo "*/10 * * * * root sudo bash $PWD/hodor.sh") | crontab -u root -
+echo "* * * * * sudo bash $PWD/log.sh 1000'" | crontab -u root -
+(crontab -u root -l ; echo "*/10 * * * * sudo bash $PWD/hodor.sh") | crontab -u root -
 
