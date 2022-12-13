c05feebe17bc5b1a67778eca3fec715f533577c6
neokyr <p00riel.kyrillin@gmail.com>
2022-12-13 07:38:09 -0500

    Standardise hodor

--- TP-Linux/hodor.sh
+++ TP-Linux/hodor.sh
@@ -8,7 +8,7 @@ else
 fi
 
 #Get percentage of used inode on /dev/sda1 and put it in "usedinode"
-unode=$(df -i | grep '/dev/sda1' | tr -s " " | cut -d " " -f 5 | cut -d "%" -f 1)
+unode=$(df -i / | grep / | tr -s " " | cut -d " " -f 5 | cut -d "%" -f 1)
 
 if (( $unode > $maxuse )) && [ $USER = "root" ]
 then
